404367596e729c2d7d87dd81baddcc8c0040cc5e
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2023-10-18 23:21:07 -0700

    fix missing dependencies in build

--- remote-client/package-app.sh
+++ remote-client/package-app.sh
@@ -1,4 +1,6 @@
 pyinstaller --name 'Syncify' \
             --icon 'img/syncify.icns' \
             --windowed  \
+            --add-data='venv/lib/python3.11/site-packages:.' \
+            --path venv/lib/python3.11/site-packages \
             syncify.py
