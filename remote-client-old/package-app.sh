8ee7815e709d0c9947ed971226551768496ee2b7
Rishi Roy <72419270+roy-rishi@users.noreply.github.com>
2023-11-30 16:20:07 -0800

    remove old client written in python

--- remote-client-old/package-app.sh
+++
@@ -1,8 +0,0 @@
-pyinstaller --name 'Syncify' \
-            --icon 'img/syncify.icns' \
-            --windowed  \
-            --add-data='venv/lib/*/site-packages:.' \
-            --path venv/lib/*/site-packages \
-            --add-data='img/syncify.png:img/' \
-            --noconfirm \
-            syncify.py
